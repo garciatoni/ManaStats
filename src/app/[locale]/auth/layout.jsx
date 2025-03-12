40b46225543f1ffa1a5317bd47608bf06678ef99
garciatoni <garciabarreratoni@gmail.com>
2025-03-12 17:43:45 +0100

    .env

--- src/app/[locale]/auth/layout.jsx
+++ src/app/[locale]/auth/layout.jsx
@@ -10,3 +10,19 @@
 }
 
 export default AuthLayout;
+
+// # Environment variables declared in this file are automatically made available to Prisma.
+// # See the documentation for more detail: https://pris.ly/d/prisma-schema#accessing-environment-variables-from-the-schema
+
+// # Prisma supports the native connection string format for PostgreSQL, MySQL, SQLite, SQL Server, MongoDB and CockroachDB.
+// # See the documentation for all the connection string options: https://pris.ly/d/connection-strings
+
+
+// DATABASE_URL="postgresql://manastats_admin_toni:purpura123**@localhost:5432/mana_stats"
+
+
+
+// DB_USER=manastats_admin_toni
+// DB_PASSWORD="purpura123**"
+// DB_NAME=mana_stats
+// DB_PORT=5432
